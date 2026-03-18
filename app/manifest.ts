49613539f7a8ec04021ef8732b07f46b68eee3db
Heikki Kukkonen <heikki.kukkonen@mafy.fi>
2026-03-18 13:55:40 +0200

    Improve Android PWA manifest compatibility

--- app/manifest.ts
+++ app/manifest.ts
@@ -17,15 +17,16 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/pwa-icon-192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "maskable"
+        purpose: "any maskable"
       },
       {
         src: "/pwa-icon-512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "maskable"
+        purpose: "any maskable"
       }
     ],
+    prefer_related_applications: false,
     share_target: {
       action: "/share/image",
       method: "post",
